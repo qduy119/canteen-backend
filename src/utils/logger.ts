2e9e5c6bba7b995aabcf0453d95e3f71a290a708
21120440 <duyialypro@gmail.com>
2024-11-19 15:04:52 +0700

    update: logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,7 +1,8 @@
 import envConfig from '@/config';
 import winston from 'winston';
+import 'winston-daily-rotate-file';
 
-const { combine, timestamp, json } = winston.format;
+const { combine, timestamp, printf } = winston.format;
 
 export const logger = winston.createLogger({
   level: envConfig.LOG_LEVEL,
@@ -9,15 +10,33 @@ export const logger = winston.createLogger({
     timestamp({
       format: 'YYYY-MM-DD hh:mm:ss.SSS A'
     }),
-    json()
+    printf(({ level, message, context, requestId, timestamp, metadata }) => {
+      return `${timestamp}::${level}::${context}::${requestId}::${message}::${JSON.stringify(metadata)}}`;
+    })
   ),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'logs/warn.log', level: 'warn' }),
-    new winston.transports.File({ filename: 'logs/info.log', level: 'info' }),
-    new winston.transports.File({ filename: 'logs/http.log', level: 'http' }),
-    new winston.transports.File({ filename: 'logs/debug.log', level: 'debug' })
+    ...['info', 'error'].map((level) => {
+      return new winston.transports.DailyRotateFile({
+        dirname: 'logs',
+        filename: `%DATE%-${level}.log`,
+        datePattern: 'YYYY-MM-DD',
+        zippedArchive: true,
+        maxSize: '1m',
+        maxFiles: '7d',
+        format: combine(
+          timestamp({
+            format: 'YYYY-MM-DD hh:mm:ss.SSS A'
+          }),
+          printf(
+            ({ level, message, context, requestId, timestamp, metadata }) => {
+              return `${timestamp}::${level}::${context}::${requestId}::${message}::${JSON.stringify(metadata)}}`;
+            }
+          )
+        ),
+        level
+      });
+    })
   ],
   exceptionHandlers: [
     new winston.transports.File({ filename: 'logs/exception.log' })
